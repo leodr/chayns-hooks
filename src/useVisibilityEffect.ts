7ba55403e527aa21afee44be95488b9bfe436d93
Leonhard Driesch <leo.driesch@gmail.com>
2020-08-12 12:28:14 +0200

    Improve and add more documentation

--- src/useVisibilityEffect.ts
+++ src/useVisibilityEffect.ts
@@ -1,7 +1,23 @@
 import { useEffect } from 'react'
 
+/**
+ * A function that can respond to the change of the visibility status of your page.
+ *
+ * @param isShown This parameter expresses wether the last visibility status update
+ *      indicates that the page is now shown or not.
+ */
 type VisibilityEffect = (isShown: boolean) => void
 
+/**
+ * This hook can execute side effects when the visibility status of your page changes.
+ *
+ * @returns Nothing. Use it similarly to `useEffect`.
+ *
+ * @param effect This function will be called when the visibility changes and can
+ *      respond to the change by executing side effects. Consider using `useCallback`
+ *      with this function, otherwise the listener will be attached and detached on
+ *      every render.
+ */
 export function useVisibilityEffect(effect: VisibilityEffect): void {
 	useEffect(() => {
 		const listener: chayns.ActivationListener = (update) => {
